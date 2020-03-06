3c3b64146a74fe04b362d080edfd216d1aeb39db
Ante Plecas <anteplecas@Antes-MacBook-Pro.local>
2020-03-05 19:59:35 -0600

    Added search bar functionality and scope bar

--- Documents Core Data/DocumentTableViewController.swift
+++ Documents Core Data/DocumentTableViewController.swift
@@ -9,127 +9,152 @@
 import UIKit
 import CoreData
 
-class DocumentTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-       @IBOutlet weak var documentsTableView: UITableView!
-       let dateFormatter = DateFormatter()
-       var documents = [Document]()
-       let searchController = UISearchController(searchResultsController: nil)
+class DocumentsTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchResultsUpdating, UISearchBarDelegate {
+   
+    @IBOutlet weak var documentsTableView: UITableView!
+    let dateFormatter = DateFormatter()
+    var documents = [Document]()
+    var searchController : UISearchController?
+    var selectedSearchScope = SearchScope.all
 
-       override func viewDidLoad() {
-           super.viewDidLoad()
-           
-           title = "Documents"
-
-           dateFormatter.dateStyle = .medium
-           dateFormatter.timeStyle = .medium
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-           setupSearchController()
-       }
-       
-       override func viewWillAppear(_ animated: Bool) {
-           fetchDocuments()
-           documentsTableView.reloadData()
-       }
-       
-       func alertNotifyUser(message: String) {
+        title = "Documents"
+
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .medium
+        
+        setupSearchController()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        fetchDocuments(searchString: "")
+    }
+    
+    func alertNotifyUser(message: String) {
         let alert = UIAlertController(title: "Alert", message: message, preferredStyle: UIAlertController.Style.alert)
         alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel) {
-               (alertAction) -> Void in
-               print("OK selected")
-           })
-           
-           self.present(alert, animated: true, completion: nil)
-       }
-       
-       func fetchDocuments() {
-           guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
-               return
-           }
-           let managedContext = appDelegate.persistentContainer.viewContext
-           let fetchRequest: NSFetchRequest<Document> = Document.fetchRequest()
-           fetchRequest.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)] // order results by document name ascending
-           
-           do {
-               documents = try managedContext.fetch(fetchRequest)
-           } catch {
-               alertNotifyUser(message: "Fetch for documents could not be performed.")
-               return
-           }
-       }
-       
-       func deleteDocument(at indexPath: IndexPath) {
-           let document = documents[indexPath.row]
-           
-           if let managedObjectContext = document.managedObjectContext {
-               managedObjectContext.delete(document)
-               
-               do {
-                   try managedObjectContext.save()
-                
-                   self.documents.remove(at: indexPath.row)
-                
-                   documentsTableView.deleteRows(at: [indexPath], with: .automatic)
-               } catch {
-                   alertNotifyUser(message: "Delete failed.")
-                   documentsTableView.reloadData()
-               }
-           }
-       }
-       
-       func numberOfSections(in tableView: UITableView) -> Int {
-           return 1
-       }
-       
-       func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-           return documents.count
-       }
-       
-       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-           let cell = tableView.dequeueReusableCell(withIdentifier: "documentCell", for: indexPath)
-           
-           if let cell = cell as? DocumentCell {
-               let document = documents[indexPath.row]
-               cell.nameLabel.text = document.name
-               cell.sizeLabel.text = String(document.size) + " bytes"
-               
-               if let modifiedDate = document.modifiedDate {
-                   cell.modLabel.text = dateFormatter.string(from: modifiedDate)
-               } else {
-                   cell.modLabel.text = "unknown"
-               }
-           }
-           
-           return cell
-       }
-
-       override func didReceiveMemoryWarning() {
-           super.didReceiveMemoryWarning()
-           // Dispose of any resources that can be recreated.
-       }
-       
-       override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-           if let destination = segue.destination as? DocumentViewController,
-              let segueIdentifier = segue.identifier, segueIdentifier == "existingDocument",
-              let row = documentsTableView.indexPathForSelectedRow?.row {
-                   destination.document = documents[row]
-           }
-       }
-       
-     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-           if editingStyle == .delete {
-               deleteDocument(at: indexPath)
-           }
-       }
-       
-    func setupSearchController() {
-        definesPresentationContext = true
-        searchController.dimsBackgroundDuringPresentation = false
-        searchController.searchResultsUpdater = self as? UISearchResultsUpdating
-        searchController.searchBar.barTintColor = UIColor(white: 0.9, alpha: 0.9)
-        searchController.searchBar.placeholder = "Search documents"
-        searchController.hidesNavigationBarDuringPresentation = false
+            (alertAction) -> Void in
+            print("OK selected")
+        })
         
-        documentsTableView.tableHeaderView = searchController.searchBar
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func fetchDocuments(searchString: String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest: NSFetchRequest<Document> = Document.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)] // order results by document name ascending
+        
+        do {
+            if (searchString != "") {
+                switch (selectedSearchScope) {
+                case .all:
+                    fetchRequest.predicate = NSPredicate(format: "name contains[c] %@ OR content contains[c] %@", searchString, searchString)
+                case .name:
+                    fetchRequest.predicate = NSPredicate(format: "name contains[c] %@", searchString)
+                case .content:
+                    fetchRequest.predicate = NSPredicate(format: "content contains[c] %@", searchString)
+                }
+            }
+            documents = try managedContext.fetch(fetchRequest)
+            documentsTableView.reloadData()
+        } catch {
+            print("Fetch could not be performed")
+        }
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        if let searchString = searchController.searchBar.text {
+            fetchDocuments(searchString: searchString)
+        }
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        selectedSearchScope = SearchScope.scopes[selectedScope]
+        if let searchString = searchController?.searchBar.text {
+            fetchDocuments(searchString: searchString)
+        }
+    }
+    
+    func deleteDocument(at indexPath: IndexPath) {
+        let document = documents[indexPath.row]
+        
+        if let managedObjectContext = document.managedObjectContext {
+            managedObjectContext.delete(document)
+            
+            do {
+                try managedObjectContext.save()
+                self.documents.remove(at: indexPath.row)
+                documentsTableView.deleteRows(at: [indexPath], with: .automatic)
+            } catch {
+                alertNotifyUser(message: "Delete failed.")
+                documentsTableView.reloadData()
+            }
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return documents.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "documentCell", for: indexPath)
+        
+        if let cell = cell as? DocumentCell {
+            let document = documents[indexPath.row]
+            cell.nameLabel.text = document.name
+            cell.sizeLabel.text = String(document.size) + " bytes"
+            
+            if let modifiedDate = document.modifiedDate {
+                cell.modLabel.text = dateFormatter.string(from: modifiedDate)
+            } else {
+                cell.modLabel.text = "unknown"
+            }
+        }
+        
+        return cell
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? DocumentViewController,
+           let segueIdentifier = segue.identifier, segueIdentifier == "existingDocument",
+           let row = documentsTableView.indexPathForSelectedRow?.row {
+                destination.document = documents[row]
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            deleteDocument(at: indexPath)
+        }
+    }
+    
+    func setupSearchController(){
+        searchController = UISearchController(searchResultsController: nil)
+               
+               searchController?.searchResultsUpdater = self
+               searchController?.obscuresBackgroundDuringPresentation = false
+               searchController?.searchBar.placeholder = "Search Documents"
+               
+               navigationItem.searchController = searchController
+             
+               definesPresentationContext = true
+               
+               searchController?.searchBar.scopeButtonTitles = SearchScope.titles
+               searchController?.searchBar.delegate = self
     }
 }
-
---
+++ Documents Core Data/SearchScope.swift
@@ -0,0 +1,27 @@
+//
+//  SearchScope.swift
+//  Documents Core Data
+//
+//  Created by Ante Plecas on 3/5/20.
+//  Copyright © 2020 Ante Plecas. All rights reserved.
+//
+
+import Foundation
+
+enum SearchScope: String {
+    case all
+    case name
+    case content
+    
+    static var titles: [String] {
+        get {
+            return [SearchScope.all.rawValue, SearchScope.name.rawValue, SearchScope.content.rawValue]
+        }
+    }
+    
+    static var scopes: [SearchScope] {
+        get {
+            return [SearchScope.all, SearchScope.name, SearchScope.content]
+        }
+    }
+}
